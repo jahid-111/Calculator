a753b60ff79dc590ee9b5edf169b9ab2654e2af5
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2023-12-10 02:30:33 +0300

    MobileCalculator

---
+++ style.js
@@ -0,0 +1,15 @@
+
+
+const buttonClass =document.querySelectorAll('button');
+console.log(buttonClass);
+
+for (let buttonStyle of buttonClass){
+
+buttonStyle.style.background="rgb(72 71 72)";
+buttonStyle.style.fontSize="22px";
+buttonStyle.style.color="white";
+buttonStyle.style.fontWeight="bold";
+buttonStyle.style.boxShadow="5px 5px 10px 2px rgba(0,0,0,.8)"
+// buttonStyle.style.shad
+
+}
